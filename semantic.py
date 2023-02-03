df70ae5898e4e21bc90de25db1e8001637b46203
KarlieKKY <111543636+KarlieKKY@users.noreply.github.com>
2023-02-03 00:26:19 +0000

    Add files via upload

---
+++ semantic.py
@@ -0,0 +1,12 @@
+# CT1
+'''en_core_web_sm is the small version of the English model,
+designed for basic NLP tasks such as tokenization, POS tagging, and dependency parsing.'''
+
+
+'''en_core_web_md is the medium-sized version of the English model, 
+designed for more advanced NLP tasks such as named entity recognition and text classification. 
+This model includes more data and larger neural network models than the small version, 
+and therefore provides more accurate results for these more complex tasks.'''
+
+
+
